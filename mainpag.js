4fde47e8263bfb928fe5c4190b0eb39f29f51358
tomas villalba buontempo <tomasv00805@gmail.com>
2023-06-11 20:59:20 -0300

    arreglar

--- mainpag.js
+++ mainpag.js
@@ -95,7 +95,7 @@ const pintarCorreosrecividos = data => {
     //que solo se muestren 15 palabras
     templateCorreo.querySelector(".cuerpo").textContent = correo.body.split(" ").slice(0, 20).join(" ");
     //se guarda el id del correo en el boton de clase botonfavoritos
-    templateCorreo.querySelector(".botonfavoritos").dataset.id = correo.id;
+    templateCorreo.querySelector(".BotondeFavoritos").dataset.id = correo.id;
     //se clona el template para unir todas sus partes
     const clone = templateCorreo.cloneNode(true);
     //se agrega el clone al fragment
@@ -127,7 +127,7 @@ const pintarCorreosenviados = data => {
     //que solo se muestren 15 palabras
     templateCorreo.querySelector(".cuerpo").textContent = correo.body.split(" ").slice(0, 20).join(" ");
     //se guarda el id del correo en el boton de clase botonfavoritos
-    templateCorreo.querySelector(".botonfavoritos").dataset.id = correo.id;
+    templateCorreo.querySelector(".BotondeFavoritos").dataset.id = correo.id;
     //se clona el template para unir todas sus partes
     const clone = templateCorreo.cloneNode(true);
     //se agrega el clone al fragment
