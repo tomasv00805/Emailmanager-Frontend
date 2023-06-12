6ebef902494a7da166ec118249f15853b01dd401
Gabriel <70722542+GabrielPlazaola@users.noreply.github.com>
2023-06-12 20:42:50 -0300

    tests

--- tests/bandejaentrada.spec.ts
+++ tests/bandejaentrada.spec.ts
@@ -7,21 +7,28 @@ test('Ver Bandeja de Entrada', async ({ page }) => {
   await page.getByPlaceholder('Contraseña').click();
   await page.getByPlaceholder('Contraseña').fill('123');
   await page.getByRole('button', { name: 'Iniciar sesión' }).click();
+  await page.waitForTimeout(3000)
   await page.getByRole('button', { name: 'Enviar correo' }).click();
+  await page.waitForTimeout(3000)
   await page.getByPlaceholder('Para').click();
   await page.getByPlaceholder('Para').fill('juli');
   await page.getByPlaceholder('Asunto').click();
-  await page.getByPlaceholder('Asunto').fill('holaaaaaa');
+  await page.getByPlaceholder('Asunto').fill('test');
   await page.getByPlaceholder('Mensaje').click();
-  await page.getByPlaceholder('Mensaje').fill('holaaaaaaaaa');
+  await page.getByPlaceholder('Mensaje').fill('test2');
   page.once('dialog', dialog => {
     console.log(`Dialog message: ${dialog.message()}`);
     dialog.dismiss().catch(() => {});
   });
   await page.getByRole('button', { name: 'Enviar', exact: true }).click();
   await page.getByRole('button', { name: 'bandeja de enviados' }).click();
+  await page.waitForTimeout(3000)
   await page.getByText('juli').nth(0).click();
-  await page.locator('#template_correoseleccionado').getByText('juli').click();
-  await page.locator('#template_correoseleccionado').getByText('holaaaaaa', { exact: true }).click();
-  await page.locator('#template_correoseleccionado').getByText('holaaaaaaaaa').click();
+  //await page.locator('#correos').getByText('juli').click();
+  //await page.locator('#template_correoseleccionado').getByText('juli').click();
+  //await page.locator('#template_correoseleccionado').getByText('holaaaaaa', { exact: true }).click();
+  //await page.locator('#template_correoseleccionado').getByText('holaaaaaaaaa').click();
+  await expect(page.locator('#template_correoseleccionado').getByText('juli')).toBeVisible();
+  await expect(page.locator('#template_correoseleccionado').getByText('test', { exact: true })).toBeVisible();
+  await expect(page.locator('#template_correoseleccionado').getByText('test2')).toBeVisible();
 });
--- tests/verfavorito.spec.ts
+++ tests/verfavorito.spec.ts
@@ -7,7 +7,9 @@ test('Ver Favorito', async ({ page }) => {
   await page.getByPlaceholder('Correo o nombre de usuario').press('Tab');
   await page.getByPlaceholder('Contraseña').fill('123');
   await page.getByPlaceholder('Contraseña').press('Enter');
+  await page.waitForTimeout(3000)
   await page.getByRole('button', { name: 'Enviar correo' }).click();
+  await page.waitForTimeout(3000)
   await page.getByPlaceholder('Para').click();
   await page.getByPlaceholder('Para').fill('tomi');
   await page.getByPlaceholder('Para').press('Tab');
@@ -20,16 +22,18 @@ test('Ver Favorito', async ({ page }) => {
   });
   await page.getByRole('button', { name: 'Enviar', exact: true }).click();
   await page.getByRole('button', { name: 'bandeja de enviados' }).click();
-  await page.getByText('tomi').first().click();
+  await page.waitForTimeout(3000)
+  await page.getByText('tomi').nth(0).click();
+  //await page.locator('#correos').getByText('tomi').click();
   page.once('dialog', dialog => {
     console.log(`Dialog message: ${dialog.message()}`);
     dialog.dismiss().catch(() => {});
   });
-  await page.getByRole('button', { name: 'fav' }).nth(1).click();
+  await page.getByRole('button', { name: 'fav' }).nth(0).click();
   await page.getByRole('button', { name: 'bandeja de favoritos' }).click();
-  await page.getByText('hola').click();
+  await page.waitForTimeout(3000)
   await page.getByText('gabrieltomi').nth(0).click();
+  //await page.locator('#correos').getByText('hola').click();
   await page.getByText('gabriel, tomi').click();
-  await page.getByText('correo@ejemplo.com').click();
-  await page.locator('#template_correoseleccionado').getByText('test').click();
+  //await page.locator('#template_correoseleccionado').getByText('test').click();
 });
