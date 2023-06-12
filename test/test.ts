a5e4a289874fbe8e18fb9407cee333f48865d5e8
tomas villalba buontempo <tomasv00805@gmail.com>
2023-06-12 03:46:46 -0300

    inicio de los test

--- test/test.ts
+++ test/test.ts
@@ -1,27 +1,28 @@
-//importa el Emailmanager
-import { EmailManager } from "../class/Emailmanager";
-import {Getjson} from "../class/Eljson";
-//creame el emailmanager y crea dos usuarios
+import { test, expect } from '@playwright/test';
 
-let emailManager = new EmailManager([]);
-
-fetch("../api2.json")
-  .then(response => response.json())
-  .then(data => {
-
-    console.log(data);
-
-
-    console.log(data.nombre);
-    console.log(data.edad);
-  
-})
-
-
-//console.log(Getjson(emailManager))
-
-
-//crear usuario
-/*emailManager.enviarCorreo("pepe@gmail.com", ["usuario2@gmail.com","usuario1@gmail.com"],"hola","chau");
-
-console.log(emailManager.usuarios);*/
+test('test', async ({ page }) => {
+  await page.goto('https://emailmanager-frontend-phi.vercel.app/');
+  await page.getByPlaceholder('Correo o nombre de usuario').click();
+  await page.getByPlaceholder('Correo o nombre de usuario').fill('tomi');
+  await page.getByPlaceholder('Correo o nombre de usuario').press('Tab');
+  await page.getByPlaceholder('Contraseña').fill('123');
+  await page.getByRole('button', { name: 'Iniciar sesion' }).click();
+  await page.getByRole('button', { name: 'Enviar correo' }).click();
+  await page.locator('#correopara').click();
+  await page.locator('#correopara').fill('tomi');
+  await page.locator('#correopara').press('Tab');
+  await page.getByPlaceholder('Asunto').fill('hola');
+  await page.getByPlaceholder('Asunto').press('Tab');
+  await page.getByPlaceholder('Mensaje').fill('hola');
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Enviar', exact: true }).click();
+  await page.getByRole('button', { name: 'bandeja de enviados' }).click();
+  await page.getByText('tomi').nth(2).click();
+  await page.locator('#template_correoseleccionado').getByText('tomi').click();
+  await page.getByText('correo@ejemplo.com').click();
+  await page.getByText('hola').nth(3).click();
+  await page.getByText('hola').nth(4).click();
+});
