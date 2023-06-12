9d4a01969c153dd9d1af55d50da5ae92bc17205a
Gabriel <70722542+GabrielPlazaola@users.noreply.github.com>
2023-06-12 04:19:12 -0300

    cosas de tests

---
+++ tests/test1.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  let hola: string = "hola";
+  await page.goto('https://emailmanager-frontend-phi.vercel.app/');
+  await page.getByPlaceholder('Correo o nombre de usuario').click();
+  await page.getByPlaceholder('Correo o nombre de usuario').fill('tomi');
+  await page.getByPlaceholder('Correo o nombre de usuario').press('Tab');
+  await page.getByPlaceholder('Contraseña').fill('123');
+  await page.getByRole('button', { name: 'Iniciar sesion' }).click();
+  await page.getByRole('button', { name: 'Enviar correo' }).click();
+  //await page.locator('#correopara').click();
+  //await page.locator('#correopara').fill('tomi');
+  //await page.locator('#correopara').press('Tab');
+  await page.getByPlaceholder('Para').click();
+  await page.getByPlaceholder('Para').fill('tomi');
+  await page.getByPlaceholder('Para').press('Tab');
+  await page.getByPlaceholder('Asunto').fill('hola');
+  await page.getByPlaceholder('Asunto').press('Tab');
+  await page.getByPlaceholder('Mensaje').fill('hola');
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Enviar', exact: true }).click();
+  await page.getByRole('button', { name: 'bandeja de enviados' }).click();
+  await page.getByText('tomi').nth(2).click();
+  await page.locator('#template_correoseleccionado').getByText('tomi').click();
+  await page.getByText('correo@ejemplo.com').click();
+  await page.getByText('hola').nth(3).click();
+  await page.getByText('hola').nth(4).click();
+});
